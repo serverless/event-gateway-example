5c7e7804d2cd187e0e35f38dd0c571f08e3e7cae
Nik Graf <nik@nikgraf.com>
2017-08-11 20:49:34 +0200

    style frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import logo from './logo.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome</h2>
+      <div>
+        <div className="nav center">
+          <div className="architecture">serverless architecture</div>
+        </div>
+        <div className="content">
+          <img src={logo} alt="logo" height="84" />
+          <h1 className="architecture center">serverless architecture</h1>
+          <div className="tagline center">the fast way to get stuff done</div>
+          <div className="center">
+            <h2>build value, fast</h2>
+          </div>
+          <div className="center">
+            There is no faster way to get new projects built and to the market
+          </div>
+          <button type="button">learn more</button>
+          <div className="center">
+            <h2>minimize overhead</h2>
+          </div>
+          <div className="center">
+            Serverless architectures are auto-scaling with pay-per-execution
+            pricing, reducing overhead like never before
+          </div>
+          <button type="button">learn more</button>
+          <div className="center">
+            <h2>make multi-cloud accessible</h2>
+          </div>
+          <div className="center">
+            Serverless architectures are auto-scaling with pay-per-execution
+            pricing, reducing overhead like never before
+          </div>
+          <button type="button">learn more</button>
         </div>
       </div>
     );
