4834bd09af08059e399fc9a1efe0fd44aa010ab0
Rupak Ganguly <rupakg@gmail.com>
2017-08-12 14:05:21 -0400

    Fix UI styling.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,66 +26,77 @@ class App extends Component {
     return (
       <div>
         <div className="nav center">
-          <div className="architecture">serverless architecture</div>
+            <span className="logo-small"><img src={logo} alt="logo" height="20" /></span>
+            <div className="architecture header-text">serverless architecture</div>
           {this.props.session &&
-            <button type="button" onClick={() => this.props.logout()}>
+            <button className="flat" type="button" onClick={() => this.props.logout()}>
               logout
             </button>}
         </div>
-        <div className="content">
+        <div className="content center">
           <img src={logo} alt="logo" height="84" />
           <h1 className="architecture center">serverless architecture</h1>
           <div className="tagline center">the fast way to get stuff done</div>
-          <div>
+          <div className="section center box">
             {this.props.session
-              ? <div>thanks for registering</div>
+              ? <div className="message">thanks for registering!</div>
               : <form onSubmit={this.register}>
                   <input
                     type="email"
                     onChange={this.updateEmail}
                     value={this.state.email}
+                    placeholder="enter your email"
                   />
-                  <button>learn more</button>
+                  <button className="flat register">register</button>
                 </form>}
           </div>
-          <div className="center">
-            <h2>build value, fast</h2>
+          <div className="section center">
+            <div>
+              <h2>build value, fast</h2>
+            </div>
+            <div>
+              There is no faster way to get new projects built and to the market
+            </div>
+            <button 
+              className="flat"
+              type="button"
+              onClick={() => this.props.logActivity('build-value')}
+            >
+              learn more
+            </button>
           </div>
-          <div className="center">
-            There is no faster way to get new projects built and to the market
+          <div className="section center">
+            <div>
+              <h2>minimize overhead</h2>
+            </div>
+            <div>
+              Serverless architectures are auto-scaling with pay-per-execution
+              pricing, reducing overhead like never before
+            </div>
+            <button
+              className="flat"
+              type="button"
+              onClick={() => this.props.logActivity('minimize-overhead')}
+            >
+              learn more
+            </button>
           </div>
-          <button
-            type="button"
-            onClick={() => this.props.logActivity('build-value')}
-          >
-            learn more
-          </button>
-          <div className="center">
-            <h2>minimize overhead</h2>
+          <div className="section center">          
+            <div>
+              <h2>make multi-cloud accessible</h2>
+            </div>
+            <div>
+              Serverless architectures are auto-scaling with pay-per-execution
+              pricing, reducing overhead like never before
+            </div>
+            <button
+              className="flat"
+              type="button"
+              onClick={() => this.props.logActivity('multi-cloud')}
+            >
+              learn more
+            </button>
           </div>
-          <div className="center">
-            Serverless architectures are auto-scaling with pay-per-execution
-            pricing, reducing overhead like never before
-          </div>
-          <button
-            type="button"
-            onClick={() => this.props.logActivity('minimize-overhead')}
-          >
-            learn more
-          </button>
-          <div className="center">
-            <h2>make multi-cloud accessible</h2>
-          </div>
-          <div className="center">
-            Serverless architectures are auto-scaling with pay-per-execution
-            pricing, reducing overhead like never before
-          </div>
-          <button
-            type="button"
-            onClick={() => this.props.logActivity('multi-cloud')}
-          >
-            learn more
-          </button>
         </div>
       </div>
     );
