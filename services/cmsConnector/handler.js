777aa30dacf797923f4e7c5d06667037aa4f2f79
Nik Graf <nik@nikgraf.com>
2017-08-14 16:29:24 +0200

    add example to track functionProviderErrors

---
+++ services/cmsConnector/handler.js
@@ -0,0 +1,3 @@
+module.exports.addUserToCms = (event, context, callback) => {
+  throw new Error('Can\'t add user to the CMS')
+}
---
+++ services/system/handler.js
@@ -0,0 +1,3 @@
+module.exports.trackFunctionProviderError = (event, context, callback) => {
+  callback(null, { message: 'Admin informed.' })
+}
